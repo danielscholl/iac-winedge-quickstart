f997d20c377d1e25455e54b99e7547fc5a208c69
Daniel Scholl <dascholl@microsoft.com>
2019-06-06 12:10:39 -0500

    backout script

--- scripts/configure-edge.ps1
+++ scripts/configure-edge.ps1
@@ -4,7 +4,7 @@
 #>
 
 Param (
-    [string]$DeviceConnectionString
+
 )
 
 # Firewall
@@ -19,7 +19,9 @@ Install-WindowsFeature Containers
 
 # Install IoT Edge
 . {Invoke-WebRequest -useb aka.ms/iotedge-win} | Invoke-Expression; `
-    Deploy-IoTEdge -ContainerOs Windows
+    Deploy-IoTEdge -ContainerOs Windows -RestartIfNeeded
+
+exit
 
 . {Invoke-WebRequest -useb aka.ms/iotedge-win} | Invoke-Expression; `
     Install-IoTEdge -Manual -DeviceConnectionString $DeviceConnectionString -ContainerOs Windows -Force -RestartIfNeeded
