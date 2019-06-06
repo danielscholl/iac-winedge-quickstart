f0ebd83213c90c5cb77dcd333034aecb66ddd0af
Daniel Scholl <dascholl@microsoft.com>
2019-06-06 10:43:04 -0500

    Updated Script

--- scripts/configure-edge.ps1
+++ scripts/configure-edge.ps1
@@ -18,9 +18,8 @@ New-NetFirewallRule -DisplayName "IoT Edge" -Direction Outbound -LocalPort 443,8
 Install-WindowsFeature Containers
 
 # Install IoT Edge
-# . {Invoke-WebRequest -useb aka.ms/iotedge-win} | Invoke-Expression; `
-# Deploy-IoTEdge -ContainerOs Windows -RestartIfNeeded
-
+. {Invoke-WebRequest -useb aka.ms/iotedge-win} | Invoke-Expression; `
+Deploy-IoTEdge -ContainerOs Windows -RestartIfNeeded
 
 . {Invoke-WebRequest -useb aka.ms/iotedge-win} | Invoke-Expression; `
     Install-IoTEdge -Manual -DeviceConnectionString $connectionString -ContainerOs Windows -Force -RestartIfNeeded
