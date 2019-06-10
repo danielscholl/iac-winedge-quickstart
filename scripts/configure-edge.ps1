05588340e99a154f4f31ea79aacd63d315694c8e
Daniel Scholl <daniel.scholl@microsoft.com>
2019-06-10 13:19:14 -0500

    Edge Deployment installs containers

--- scripts/configure-edge.ps1
+++ scripts/configure-edge.ps1
@@ -10,12 +10,9 @@ Param (
 # Firewall
 ## Inbound Ports required 15580, 15581
 New-NetFirewallRule -DisplayName "IoT Edge" -Direction Inbound -LocalPort 15580,15581 -Protocol TCP -Action Allow
- 
+
 ## Outbound Ports required 443, 8883, 5671
 New-NetFirewallRule -DisplayName "IoT Edge" -Direction Outbound -LocalPort 443,8883,5671 -Protocol TCP -Action Allow
-
-# Install Containers
-Install-WindowsFeature Containers
 
 # Deploy IoT Edge
 . {Invoke-WebRequest -useb aka.ms/iotedge-win} | Invoke-Expression; `
